1a3e89bc73a36576bf3b8c8e07de498ede7f49bf
SujinHuh <juin483@naver.com>
2024-01-22 15:23:29 +0900

    first commit

---
+++ src/HelloJava.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;/**
+ *packageName    : PACKAGE_NAME
+ * fileName       : HelloJava
+ * author         : keti
+ * date           : 2024-01-22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-01-22        keti       최초 생성
+ */public class HelloJava {
+}
---
+++ src/method/Method1.java
@@ -0,0 +1,12 @@
+package method;/**
+ *packageName    : method
+ * fileName       : Method1
+ * author         : keti
+ * date           : 2024-01-22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-01-22        keti       최초 생성
+ */public class Method1 {
+}
